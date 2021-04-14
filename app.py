f97c2beb6df3149e95e0c410aec05399f298aaab
urban-us-co <stonly@urban.us>
2021-04-14 13:34:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,14 +21,15 @@ fig.add_trace(go.Sunburst(
     ids=df1.ids,
     labels=df1.labels,
     parents=df1.parents,
-    domain=dict(column=0),
-    insidetextorientation='radial'
+    domain=dict(column=0)
 ))
 
 
 fig.update_layout(
     grid= dict(columns=2, rows=1),
-    margin = dict(t=0, l=0, r=0, b=0)
+    margin = dict(t=0, l=0, r=0, b=5),
+    uniformtext = dict(minsize=20, mode='hide')
+
 )
 
 #fig.show()
