5ad7a232322ccd543bdc5d1ffa13a7d8e302cbf3
urban-us-co <stonly@urban.us>
2021-04-13 17:16:30 -0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,17 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.graph_objects as go
 
 import pandas as pd
+
+########### Define your variables
+color1='darkred'
+color2='orange'
+mytitle='UU Radar'
+tabtitle='...'
+myheading='Urban Us Radar'
+
 
 df1 = pd.read_csv('https://storage.googleapis.com/uu_public/radar_data4.csv')
 
@@ -19,4 +30,25 @@ fig.update_layout(
     margin = dict(t=0, l=0, r=0, b=0)
 )
 
-fig.show()
+#fig.show()
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+########### Set up the layout
+app.layout = html.Div(children=[
+    html.H1(myheading),
+    dcc.Graph(
+        id='uu-radar',
+        figure=fig
+    ),
+    html.A('Urban Us', href='https://urban.us'),
+    html.Br(),
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server()
