dfd48d69a9bea01f06a027d07e30b0047f7c401e
urban-us-co <stonly@urban.us>
2021-04-26 17:20:04 -0700

    big push to add filtering

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+"""
+
+TODO
+https://cmdlinetips.com/2018/02/how-to-subset-pandas-dataframe-based-on-values-of-a-column/
+https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+
+"""
+
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -6,28 +15,36 @@ import dash_table
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 
+
 import json
 import pandas as pd
 
 ########### Define your variables
 color1='darkred'
 color2='orange'
-mytitle='Urban Us Radar'
-tabtitle='Urban Us Radar'
-myheading='Scope Emissions and Resilience Solutions Radar'
+mytitle='Urban Us Climate Solutions Radar'
+tabtitle='Urban Us Climate Solutions Radar'
+myheading='Climate Solutions Radar'
 
 
 df1 = pd.read_csv('https://raw.githubusercontent.com/urban-us-co/uu-radar/main/radar_data.csv')
 #details = pd.read_json(url)
+
+new_graph_data = pd.DataFrame(columns=['ids', 'labels', 'parents', 'descriptions', 'website_1', 'jobs_id'])
 
 #df2 = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 
 df_dict = df1.to_dict(orient='records')
 
 companies_dict = dict()
+companies_list = []
+companies_options = []
 
 for i in df_dict:
     companies_dict[i['labels']] = i['website_1']
+    if len(i['website_1']) > 2 and i['labels'] not in companies_list:
+        companies_list.append(i['labels'])
+        companies_options.append({'label': i['labels'], 'value': i['labels']})
 
 styles = {
     'pre': {
@@ -36,28 +53,6 @@ styles = {
     }
 }
 
-fig = go.Figure()
-
-fig.add_trace(go.Sunburst(
-    ids=df1.ids,
-    labels=df1.labels,
-    parents=df1.parents,
-    customdata=df1.descriptions,
-    name='',
-    domain=dict(column=0),
-    #hovertemplate = '<b>%{label} - %{customdata}</b><br>',
-    insidetextorientation='radial',
-))
-
-
-fig.update_layout(
-    grid= dict(columns=1, rows=1),
-    margin = dict(t=0, l=0, r=0, b=0),
-    clickmode='event+select',
-    #uniformtext = dict(minsize=10, mode='hide')
-)
-
-#fig.show()
 
 ########### Initiate the app
 external_stylesheets = ['https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/cosmo/bootstrap.min.css']
@@ -69,6 +64,32 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets, external_sc
 server = app.server
 app.title=tabtitle
 
+
+######### draw initial graph
+
+def draw_graph(graph_data):
+    fig = go.Figure()
+
+    fig.add_trace(go.Sunburst(
+        ids=graph_data.ids,
+        labels=graph_data.labels,
+        parents=graph_data.parents,
+        customdata=graph_data.descriptions,
+        name='',
+        domain=dict(column=0),
+        #hovertemplate = '<b>%{label} - %{customdata}</b><br>',
+        insidetextorientation='radial',
+    ))
+
+
+    fig.update_layout(
+        grid= dict(columns=1, rows=1),
+        margin = dict(t=0, l=0, r=0, b=0),
+        clickmode='event+select',
+        #uniformtext = dict(minsize=10, mode='hide')
+    )
+
+    return fig
 
 
 ########## Custom index to enable google analytics and other things
@@ -103,6 +124,19 @@ app.index_string = '''<!DOCTYPE html>
 
 
 
+########### Set up dropdown
+
+dropdown = html.Div([
+    dcc.Dropdown(
+        id='startup-dropdown',
+        options=companies_options,
+        value=[],
+        multi=True,
+        placeholder='You can also select startups to filter the chart...'
+    ),
+    html.Div(id='dd-output-container')
+])
+
 
 ########### Set up the layout
 
@@ -116,6 +150,8 @@ info_card =  dbc.Row(dbc.Col([
                         Click on any part of the graph to get more information (shown below) and to zoom in and change the center of the graph. Click on the center of the graph to zoom back out.
                     """),
                     html.Br(), 
+                    dropdown,
+                    html.Br(),
                 ], className=''),
         dbc.Col(id='show-data'),
     ]),
@@ -154,13 +190,12 @@ app.layout = dbc.Container(
     [
         html.H1(myheading, style={'textAlign': 'center'}),
         html.Hr(),
+
         dbc.Row(
             [                
                 dbc.Col(
                     dcc.Graph(
                         id='uu-radar-chart',
-                        figure=fig,
-                        responsive=True
                     ),md=8),
                 dbc.Col(info_card, md=4),
             ],
@@ -169,9 +204,9 @@ app.layout = dbc.Container(
         dbc.Row(
             [                
                 dbc.Col([
-                    html.Br(), 
                     html.Br(),
-                ]),
+                    html.Br(), 
+                ], md=8),
             ]
         ),
         dbc.Row(
@@ -208,6 +243,8 @@ def display_click_data(clickData):
     jobs_link = ""
     contact_text = ""
     contact_link = ""
+    if not clickData: 
+        return ""
     item_id = clickData["points"][0]["id"]
     item_name = clickData["points"][0]["label"]
     item_desc = clickData["points"][0]["customdata"]
@@ -251,6 +288,47 @@ def display_click_data(clickData):
 
     return return_card
 
+
+@app.callback(
+    dash.dependencies.Output('uu-radar-chart', 'figure'),
+    [dash.dependencies.Input('startup-dropdown', 'value')])
+def update_graph(value):
+    graph_data = df1
+    if len(value) == 0:
+        return draw_graph(graph_data)
+    else:
+        print("Reload graph data....")
+
+    #Index(['ids', 'labels', 'parents', 'descriptions', 'website_1', 'jobs_id'], dtype='object')
+
+    parents = []
+    current_parent = ""
+    #print(graph_data)
+    for v in value:
+        #print(v)
+        parents.append(v)
+        this_row_parent = ""
+        this_row_parent = graph_data.loc[graph_data['ids']==v, 'parents'].item()
+        #print(this_row_parent)
+        this_id = ""
+        while len(this_row_parent) > 1:
+            this_id = graph_data.loc[graph_data['ids']==this_row_parent, 'ids'].item()
+            parents.insert(0, this_id)
+            this_row_parent = ""
+            this_row_parent = graph_data.loc[graph_data['ids']==this_id, 'parents'].item()
+            #print(str(this_row_parent))
+            if str(this_row_parent) == "nan":
+                this_row_parent = "" 
+            this_id = ""
+            #print(this_row_parent)
+    #print(parents)
+    new_graph_data = graph_data[graph_data["ids"].isin(parents)]
+    #print(new_graph_data)
+
+    #print(parents)
+
+    return draw_graph(new_graph_data)
+    #return 'You have selected "{}"'.format(value)
 
 
 if __name__ == '__main__':
